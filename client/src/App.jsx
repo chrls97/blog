53b16090707459a2061a74c8ae2cc64a9823aedb
charles <charlesgintoki@gmail.com>
2025-09-13 23:32:18 +0800

    added register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
+import Register from './pages/Register'
 
 const App = () => {
 
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route path='/' element={<Register />} />
       </Routes>
     </div>
   )
